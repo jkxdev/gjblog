c18217f09e815e5569e22e9d595b23f3689d865a
Josie <jkokkat@cisco.com>
2017-02-02 23:50:03 +0530

    initial one

---
+++ src/main/java/com/cmad/cmad/App.java
@@ -0,0 +1,13 @@
+package com.cmad.cmad;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/cmad/cmad/model/UserDetail.java
@@ -0,0 +1,50 @@
+package com.cmad.cmad.model;
+
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Id;
+
+@Entity
+public class UserDetail {
+
+	@Id
+	private String id = new ObjectId().toString();
+	
+	private String username;
+	private String pwd;
+	private String fullName;
+	private String phno;
+	
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullname) {
+		this.fullName = fullname;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "UserDetail  username "+username+",fullname "+", pwd "+pwd;
+	}
+	public String getPhno() {
+		return phno;
+	}
+	public void setPhno(String phno) {
+		this.phno = phno;
+	}
+	
+}
